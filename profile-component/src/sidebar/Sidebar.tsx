15476202c17434a9be9fa457b3eefbfff5082f2b
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-13 23:00:07 +0530

    made it a bit mobile responsive

--- profile-component/src/sidebar/Sidebar.tsx
+++ profile-component/src/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@
 const Sidebar = () => {
   return (
     <div className=" flex items-center justify-between gap-10">
-      <div className="h-screen w-60 rounded-2xl bg-neutral-200"/>
+      <div className="h-screen hidden md:block w-60 rounded-2xl bg-neutral-200"/>
       <div className="h-screen w-full rounded-2xl bg-green-200"/>
     </div>
   )
