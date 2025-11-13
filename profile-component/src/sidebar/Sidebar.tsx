7c7144221970af16171032aa908e8bfba59830dd
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-13 23:36:29 +0530

    added animations

--- profile-component/src/sidebar/Sidebar.tsx
+++ profile-component/src/sidebar/Sidebar.tsx
@@ -2,8 +2,8 @@
 
 const Sidebar = () => {
   return (
-    <div className=" flex items-center justify-between gap-10">
-      <div className="h-screen hidden md:block w-60 rounded-2xl bg-neutral-200"/>
+    <div className=" flex items-center justify-between">
+      <div className="h-screen w-0 transition-all duration-1000 md:w-96 rounded-2xl bg-neutral-200"/>
       <div className="h-screen w-full rounded-2xl bg-green-200"/>
     </div>
   )
